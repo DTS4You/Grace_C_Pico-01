413c1b37f9ec4223336568942688985f9e36fb69
Norbert Schwarz <info@datentechnikschwarz.de>
2025-03-09 13:25:41 +0100

    Init

--- module_ws2812_v2.py
+++ module_ws2812_v2.py
@@ -122,6 +122,8 @@ def setup_ws2812():
 
     print(mg.numpix_1)
     print(mg.numpix_2)
+    print(mg.seg_01_count)
+    print(mg.seg_02_count)
     
     strip_obj.append(module_neopixel.Neopixel(mg.numpix_1, 0, 2, "GRB"))
     strip_obj.append(module_neopixel.Neopixel(mg.numpix_2, 1, 3, "GRB"))
